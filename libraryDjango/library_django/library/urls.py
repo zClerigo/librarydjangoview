b8315f67f89f2dfaa4bbafd12dd2c13ac1ed063f
rex <zidanni.clerigo@gmail.com>
2024-04-16 23:13:49 -0400

    auth

--- libraryDjango/library_django/library/urls.py
+++ libraryDjango/library_django/library/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
+from django.conf.urls import include
 from . import views
 
 app_name = "movies"
 
 urlpatterns = [
     path("home/", views.HomePageView.as_view(), name="home"),
-    path("checkout/", views.CheckoutView.as_view(), name="checkout")
+    path("checkout/", views.CheckoutView.as_view(), name="checkout"),
 ]
